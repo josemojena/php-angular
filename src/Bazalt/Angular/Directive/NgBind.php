ad1b834e51d082ca5f48261fdea3a8a897d9cb02
jose <jose31309>
2018-10-31 16:08:12 +0100

    Added support to include string inside ng-bind expressions

--- src/Bazalt/Angular/Directive/NgBind.php
+++ src/Bazalt/Angular/Directive/NgBind.php
@@ -8,26 +8,27 @@ class NgBind extends \Bazalt\Angular\Directive
     protected function parseValue($matches)
     {
 
+
         $key = trim($matches['value']);
         $filters = isset($matches['filters']) ? explode('|', trim($matches['filters'], ' |')) : [];
 
         $value = $this->scope->getValue($key);
-        
 
         if (!$value && $value != '') {
             return $matches[0];
         }
-        
-        foreach ( $filters as $filter ){
 
-            $filter_split = explode( ':', $filter);
+
+        foreach ($filters as $filter) {
+
+            $filter_split = explode(':', $filter);
             $filter_function = array_slice($filter_split, 0, 1);
             $filter_function = trim($filter_function[0]);
             $filter_arguments = array_slice($filter_split, 1);
             array_unshift($filter_arguments, $value);
 
-            if(array_key_exists($filter_function, $this->scope['filters'])){
-                $value = call_user_func_array($this->scope['filters'][$filter_function] , $filter_arguments);
+            if (array_key_exists($filter_function, $this->scope['filters'])) {
+                $value = call_user_func_array($this->scope['filters'][$filter_function], $filter_arguments);
             }
         }
 
@@ -36,6 +37,27 @@ class NgBind extends \Bazalt\Angular\Directive
 
     public function apply()
     {
-        $this->element->nodeValue = preg_replace_callback('|{{\s*(?<value>[a-z0-9\.]*)\s*(\|\s*(?<filters>.*))?\s*}}|im', [$this, 'parseValue'], $this->element->wholeText);
+        $text = $this->element->wholeText;
+        $specialCases = array('import-url' => false);
+
+        if (preg_match('/\{\{.*?\'@import\s*url\s*\(\s*\'/', $text)) {
+            $text = preg_replace_callback('/\{\{.*?\'@import\s*url\s*\(\s*\'\s*\+\s*(?<value>[a-z0-9A-Z\.]*)(\s*\+\s*\'\)\s*;\'\s*}}\s*)/', function ($matches) {
+                return sprintf("{{%s}}", str_replace("\s", "", $matches['value']));
+            }, $text);
+            $specialCases['import-url'] = true;
+        }
+
+        $value = preg_replace_callback('|{{\s*(?<value>[a-z0-9\.]*)\s*(\|\s*(?<filters>.*))?\s*}}|im', [
+            $this,
+            'parseValue'
+        ], $text);
+
+        foreach ($specialCases as $key => $val) {
+            if ($key == 'import-url' && $val == true) {
+                $this->element->nodeValue = sprintf("@import url('%s');", trim(str_replace("\n", "", $value)));
+            } else {
+                $this->element->nodeValue = $value;
+            }
+        }
     }
 }
